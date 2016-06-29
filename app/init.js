0851094cf5b90f45f8e7a33d05ab9c5d87aa5a93
James Hamlin <jfhamlin@gmail.com>
2016-06-29 14:56:47 -0700

    Add react-redux and react-router-redux with a route-only store.
    The store isn't yet used for anything.
    
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- app/init.js
+++ app/init.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 import routes from './routes';
+import { configureStore } from './store';
 
 require('./styles/main.scss');
 
+const store = configureStore({ routing: routerReducer });
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render((
-  <Router history={ browserHistory }>
-    {routes}
-  </Router>
+  <Provider store={store}>
+    <Router history={ history }>
+      {routes}
+    </Router>
+  </Provider>
 ), document.getElementById('root'));
